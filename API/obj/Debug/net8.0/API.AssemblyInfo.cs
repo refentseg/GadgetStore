a7d75999ae481477ddf7d2e9d14a0a8a6fa4bfd9
theuglybwe <refentse.dev@gmail.com>
2024-08-23 11:36:21 +0200

    Some cleaning

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2020a09dbb682c658b2f8136ff29661ecf5c987e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46671452e9775b6913484d4455f95a6698fb8136")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
