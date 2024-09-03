3fd871473d3c2de41a6567e63a4ad4a977841442
theuglybwe <refentse.dev@gmail.com>
2024-09-03 16:47:11 +0200

    theme change dark or light

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff4fab60273fcc50181749a1592eec061e4ad6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a63fd9240430d25106b46054648faffc6b23f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
