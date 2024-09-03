7a4ffefefc5c89f5043e8e7ea9d07b0faa055359
theuglybwe <refentse.dev@gmail.com>
2024-09-03 11:21:49 +0200

    Net8 update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a257db8b2e63f573a3f88baacd90ab4de5afaba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c86eb1300b1a16e75e5d3c58196e8cc60a15a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
