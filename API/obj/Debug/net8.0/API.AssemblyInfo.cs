2ff4fab60273fcc50181749a1592eec061e4ad6e
theuglybwe <refentse.dev@gmail.com>
2024-09-03 15:14:50 +0200

    Added client side

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999e5f36fc7de9fe56aad7d1c995a9151eb653c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a058aa8a8565bf19ffd0841129f5d4559d129a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
